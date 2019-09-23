4fcbbd0a116ec61dac84b873e968c766e7202a53
mariabrock <mariabrock414@gmail.com>
2019-09-23 18:13:20 -0500

    got convert button to work

--- main.js
+++ main.js
@@ -9,57 +9,59 @@ const snowflake = `<i class="far fa-snowflake"></i>`
 const thermometer =`<i class="fas fa-thermometer-half"></i>`
 const sunShine = `<i class="fas fa-sun"></i>`
 const smiley = `<i class="far fa-smile"></i>`
-let domString = `<div class="card" style="width: 18rem;">
-<div class="card-body">
-  <h5 class="card-title"><i class="fas fa-thermometer-half"></i></h5>
-  <p class="card-text">${"test"}</p>
-</div>
-</div>`
-const cRadio = document.getElementById("inlineRadio2");
-const fRadio = document.getElementById("inlineRadio1");
+
+const cRadio = document.getElementById("celsius");
+const fRadio = document.getElementById("fahrenheit");
+cRadio.checked = true;
+fRadio.checked = false;
 
 // Get a reference to the button element in the DOM
 const input = document.getElementById("input-temp");
 const clearButton = document.getElementById("clear");
 const convertButton = document.getElementById("convert");
 
-printToDom(domString, "output");
 
-const toCelsius =  () => {
-    const input = document.getElementById("input-temp");
+
+const toCelsius =  (input) => {
     const celsius = ((input - 32) /1.8);  // input minus 32 then divided by 1.8
-    console.log(celsius);
-    //printToDom(domString, "output");
+    return(celsius);
 }
 
 
-const toFahrenheit = () => {
-    const input = document.getElementById("input-temp");1
+const toFahrenheit = (input) => {
     let fahrenheit = ((input * 1.8) +32)  //input times 1.8 then + 32
-    console.log(fahrenheit);
-    //printToDom(domString, "output");
-
+    return(fahrenheit);
 }
 
-// // This function should determine which conversion should
-// // happen based on which radio button is selected.
-const determineConverter = (e) => {
-    const value = e.target.getElementById("input-temp");
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+const determineConverter = () => {
+    const input = document.getElementById("input-temp").value;
+    let value = 0
     if (fRadio.checked === true) {
-        toFahrenheit();
+        value = toFahrenheit(input);
     } else if (cRadio.checked === true) {
-        toCelsius();
+        value = toCelsius(input);
     }
-    //printToDOM (value, "output")
+    domBuilder(value);
 }
+
+const domBuilder = (temp) => {
+    let domString = `<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title"><i class="fas fa-thermometer-half"></i></h5>
+  <p class="card-text">${temp}</p>
+</div>
+</div>` 
+printToDom(domString, "output");
+} 
+
 // Assign a function to be executed when the button is clicked
-const init = (e) => {
-    cRadio.checked = false;
-    fRadio.checked = false;
-    temperatureInput.value = '';
-    tempResult.innerHTML = '';
+const init = () => {    
     convertButton.addEventListener("click", determineConverter);
-    clearButton.addEventListener("click", determineConverter); 
+    // clearButton.addEventListener("click"); 
+    // enterKey();
 }
+init();
 
 
